f39e1cacd8db0fc0348118770aa8aeb0d21fae41
Skyler Fly-Wilson <skyfly200@gmail.com>
2018-11-17 14:44:48 -0700

    new functions for portfolio site

--- app.js
+++ app.js
@@ -23,51 +23,86 @@ const Datastore = require('@google-cloud/datastore');
 const datastore = Datastore();
 
 // convert to async await
-async function insertSignature (req) {
-	let count = await getCount();
-	// Create a signature record to be stored in the database
-	const signature = {
-		timestamp: new Date(),
-	    // Store a hash of the visitor's ip address
-	    userIp: crypto.createHash('sha256').update(req.ip).digest('hex').substr(0, 7),
-	    count: count + 1,
-	    firstName: req.body.firstName,
-	    lastName: req.body.lastName,
-	    email: req.body.email,
-	    city: req.body.city,
-	    state: req.body.state,
-	    zip: req.body.zip,
-	    message: req.body.message
+async function insertPost (req) {
+	// Create a post record to be stored in the database
+	const post = {
+			datetime: req.body.datetime,
+	    title: req.body.title,
+	    body: req.body.body,
+	    tags: req.body.tags,
+			key: req.body.key
 	};
-  	let result = await datastore.insert({
-		key: datastore.key(['signature', signature.email]),
-		data: signature,
+  let result = await datastore.insert({
+		key: datastore.key(['post', post.key]),
+		data: post,
 	});
-	return {
-		result: result,
-		entry: signature,
-	};
+	for (tag in post.tags) tagLink(tag, post.key);
+	return result;
 }
 
-async function getEmail (email) {
-	const query = datastore.createQuery('signature').filter('email',email);
+async function tagLink (tag, post) {
+	const transaction = datastore.transaction();
+	transaction.run((err) => {
+	  if (err) {}
+	  const key = datastore.key(['tag', tag.toLowerCase()]);
+	  transaction.get(key, (err, entity) => {
+	    if (err) {}
+			let update = entity ? entity : [];
+	    update.posts.append(post);
+	    transaction.save(entity);
+	    transaction.commit((err) => {
+	      if (!err) {}
+	    });
+	  });
+	});
+}
+
+async function getPost (key) {
+	const query = datastore.createQuery('post').filter('__key__', key);
 	let result = await datastore.runQuery(query);
 	return result[0];
 }
 
+async function getPostsByTag (tag) {
+	const transaction = datastore.transaction();
+	transaction.run((err) => {
+	  if (err) {}
+		const query = datastore.createQuery('tag').filter('__key__', tag)
+	  query.run((err, entities) => {
+	    if (err) {}
+			const request = {
+			  projectId: '',
+			  keys: entities,
+			};
+			transaction.lookup(request)
+			  .then(responses => {
+			    const response = responses[0];
+			    return response;
+			  })
+			  .catch(err => {
+			    console.error(err);
+			  });
+	    transaction.commit((err) => {
+	      if (!err) {
+	        // Transaction committed successfully.
+	      }
+	    });
+	  });
+	});
+}
 
-async function getCount () {
-	const query = datastore.createQuery('signature').order('timestamp', { descending: true }).limit(1);
+async function getPosts () {
+	const query = datastore.createQuery('post').order('datetime', { descending: true });
 	let results = await datastore.runQuery(query);
     const entities = results[0];
-    if (entities[0] && entities[0].count) return entities[0].count;
+    if (entities) return entities;
     return 0;
 }
 
-app.get('/count', (req, res, next) => {
-	getCount()
-	.then((count) => {
-      res.status(200).json({count});
+app.get('/posts', (req, res, next) => {
+	getPosts()
+	.then((posts) => {
+      res.status(200).json({posts});
       next();
     })
 	.catch( (error) => {
@@ -78,7 +113,7 @@ app.get('/count', (req, res, next) => {
 });
 
 app.post('/submit', (req, res, next) => {
-	insertSignature(req)
+	insertPost(req)
 	.then( (entry) => {
 		res.status(200).json(entry);
   		next();
