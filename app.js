25c8c07a63a258667b3ad40575bcafd2f6ab4993
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2018-11-18 09:38:25 -0700

    fix post object formating
    trying to fix merge conflict

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ async function insertPost (req) {
 	    title: req.body.title,
 	    body: req.body.body,
 	    tags: req.body.tags,
-			key: req.body.id
+	    key: req.body.id
 	};
   let result = await datastore.insert({
 		key: datastore.key(['post', post.key]),
