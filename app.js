5d9e8993636eabb42fc013da42f16246609c6388
Skyler Fly-Wilson <skyfly200@gmail.com>
2018-11-20 03:57:11 -0700

    added auth endpoints

--- app.js
+++ app.js
@@ -159,6 +159,49 @@ app.post('/submit', (req, res, next) => {
 	});
 });
 
+function registerUser(req, res, next) {
+	datastore.insert({
+		key: datastore.key(['user', req.body.email]),
+		data: {
+			name: req.body.name,
+			email: req.body.email,
+			pass: bcrypt.hashSync(req.body.password, 8),
+			is-admin: 0
+		}
+	},
+	(err) => {
+		if (err) return res.status(500).send("There was a problem registering the user.")
+		sendAuth(req, res, next);
+	});
+});
+
+app.post('/register', registerUser);
+
+function sendAuth(req, res, next) {
+	getUser(req.body.email)
+	.then( (user) => {
+    if (!user) return res.status(404).send('No user found.');
+    let passwordIsValid = bcrypt.compareSync(req.body.password, user.user_pass);
+    if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
+    let token = jwt.sign({ id: user.id }, config.secret, { expiresIn: 86400 }); // Expires in 24 hours
+    res.status(200).send({ auth: true, token: token, user: user });
+		next();
+  })
+	.catch( (err) => {
+		res.status(500).send("There was a problem getting user");
+	});
+}
+
+async function getUser(email) {
+	const query = datastore.createQuery('user').filter('email', email);
+	let result = await datastore.runQuery(query);
+	const entities = result[0];
+    if (entities) return entities;
+    return 0;
+}
+
+app.post('/login', sendAuth);
+
 const PORT = process.env.PORT || 8080;
 app.listen(process.env.PORT || 8080, () => {
 	console.log(`App listening on port ${PORT}`);
