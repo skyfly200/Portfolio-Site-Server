3f11a4bd41adfd009ae16bee5d7cece49e88d1d5
Skyler Fly-Wilson <skyfly200@gmail.com>
2018-11-22 09:32:26 -0700

    fix nameing overwrite on res error on line 160

--- app.js
+++ app.js
@@ -156,8 +156,8 @@ app.get('/post/:id', (req, res, next) => {
 
 app.delete('/post/:id', (req, res, next) => {
 	deletePost(req.params.id)
-	.then((res) => {
-      res.status(200).json({response: res, id: req.params.id});
+	.then((result) => {
+      res.status(200).json({result: result, id: req.params.id});
       next();
     })
 	.catch( (error) => {
