be73edc1e70d30d5b68b2a4185b7017840f919da
Skyler Fly-Wilson <skyfly200@gmail.com>
2018-12-02 15:26:28 -0700

    add missing semi colon

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.enable('trust proxy');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.options('*', cors())
+app.options('*', cors());
 
 // By default, the client will authenticate using the service account file
 // specified by the GOOGLE_APPLICATION_CREDENTIALS environment variable and use
