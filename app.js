eb75e8c5bd730bb8a0fe809d992148abb558dd95
Skyler Fly-Wilson <skyfly200@gmail.com>
2018-12-02 16:46:56 -0700

    reorder middleware

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ const config = require('./config');
 
 const app = express();
 app.enable('trust proxy');
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
 
 // By default, the client will authenticate using the service account file
 // specified by the GOOGLE_APPLICATION_CREDENTIALS environment variable and use
