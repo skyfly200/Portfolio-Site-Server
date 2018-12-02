926f1f6785ee99d8b9d84439e12b167d7c80a8c0
Skyler Fly-Wilson <skyfly200@gmail.com>
2018-12-02 15:52:52 -0700

    add OPTIONS for cors preflight

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ app.enable('trust proxy');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.options('*', cors());
-app.use(cors());
+var corsOptions = {"methods": "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS"};
+app.options('*', cors(corsOptions));
 
 // By default, the client will authenticate using the service account file
 // specified by the GOOGLE_APPLICATION_CREDENTIALS environment variable and use
@@ -48,7 +48,6 @@ async function tagLink (tag, post) {
 }
 
 // Post Functions
-
 async function getPostsByTag (tag) {
 	const transaction = datastore.transaction();
 	transaction.run((err) => {
