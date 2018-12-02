1f447f8d50c1b186c2200d4b34725e1036bce001
Skyler Fly-Wilson <skyfly200@gmail.com>
2018-12-02 16:16:37 -0700

    remove custom cors settings

--- app.js
+++ app.js
@@ -14,9 +14,7 @@ const app = express();
 app.enable('trust proxy');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-
-var corsOptions = {"methods": "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS"};
-app.options('*', cors(corsOptions));
+app.use(cors);
 
 // By default, the client will authenticate using the service account file
 // specified by the GOOGLE_APPLICATION_CREDENTIALS environment variable and use
