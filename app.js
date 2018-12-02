c1843e0fb8db020fc1399c049205b8af9034dd20
Skyler Fly-Wilson <skyfly200@gmail.com>
2018-12-02 15:29:42 -0700

    use cors

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.options('*', cors());
+app.use(cors());
 
 // By default, the client will authenticate using the service account file
 // specified by the GOOGLE_APPLICATION_CREDENTIALS environment variable and use
