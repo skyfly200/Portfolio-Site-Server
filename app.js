1241d5b60c0d8cb95a111d8d9897f648db89f70d
Skyler Fly-Wilson <skyfly200@gmail.com>
2018-12-02 16:22:19 -0700

    oops forgot the parens on cors

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 app.enable('trust proxy');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 
 // By default, the client will authenticate using the service account file
 // specified by the GOOGLE_APPLICATION_CREDENTIALS environment variable and use
